f56b3e9683ca8c8315e77987612284426856982b
王佳文 <wangjiawen@email.com>
2023-04-14 21:04:49 +0800

    创建了项目

---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,32 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: 'https://foruda.gitee.com/avatar/1680613182692722945/10848617_jiawen999_1680613182.png!avatar200',
+      name: '王佳文',
+      description: 'Star on <a href="https://github.com/jiawen-official" class="text-blue-500" target="_blank" >Github</a>',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
